80bd4d287c3ac660383b658b4cfeaab09d21ec9e
Thanachin <143328453+ThanachinPronmmana@users.noreply.github.com>
2025-03-27 23:22:20 +0700

    final

--- routers/food.js
+++ routers/food.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const { createFoods,upload,deleteFood,getAllFoods,updateFood,} = require("../controllers/food");
+const { createFoods,
+    upload,
+    deleteFood,
+    getAllFoods,
+    updateFood,
+} = require("../controllers/food");
 
 const router = express.Router();
 
-router.get("/food",getAllFoods);      // ✅ ดึงข้อมูลอาหารทั้งหมด
+router.get("/food", getAllFoods);      // ✅ ดึงข้อมูลอาหารทั้งหมด
 router.post("/food", upload.single("images"), createFoods);    // ✅ เพิ่มอาหารใหม่
-router.put("/food/:id",updateFood);  // ✅ อัปเดตข้อมูลอาหาร
-router.delete("/food/:id",deleteFood); // ✅ ลบอาหาร
+router.put("/food/:id", updateFood);  // ✅ อัปเดตข้อมูลอาหาร
+router.delete("/food/:id", deleteFood); // ✅ ลบอาหาร
 
 module.exports = router;
