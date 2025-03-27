196bd765b802ab015ff1f3ed6a2ffcd8eb3c0246
Tehdev <kunatratchapat555@gmail.com>
2025-03-27 22:53:05 +0700

    AddFunction ImagesFood

--- routers/food.js
+++ routers/food.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const { createFoods,upload,getAllFoods,updateFood,deleteFood} = require("../controllers/food");
+const { createFoods,upload} = require("../controllers/food");
 
 const router = express.Router();
 
-router.get("/food",getAllFoods);      //  ดึงข้อมูลอาหารทั้งหมด
-router.get("/food/:id",); //  ดึงข้อมูลอาหารตาม ID
-router.post("/food", upload.single("images"), createFoods);    //  เพิ่มอาหารใหม่
-router.put("/food/:id",updateFood);  //  อัปเดตข้อมูลอาหาร
-router.delete("/food/:id",deleteFood); //  ลบอาหาร
-
-
+router.get("/food",);      // ✅ ดึงข้อมูลอาหารทั้งหมด
+router.get("/food/:id",); // ✅ ดึงข้อมูลอาหารตาม ID
+router.post("/food", upload.single("images"), createFoods);    // ✅ เพิ่มอาหารใหม่
+router.put("/food/:id",);  // ✅ อัปเดตข้อมูลอาหาร
+router.delete("/food/:id",); // ✅ ลบอาหาร
 
 module.exports = router;
